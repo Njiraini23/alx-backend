13edc867e3bff2451459371acd601ca9ea9c96ea
Njiraini23 <njiraini88@gmail.com>
2023-09-05 12:56:35 +0000

    0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """A function that takes two integers page and size
 returnd a tuple of size two"""
+from typing import Tuple
 
 
-def index_range(page: int, page size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ returns a tuple of size 2 """
     if page < 1 or page_size <= 0:
         pass
