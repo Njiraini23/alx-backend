c6b70d919f94100f121e5e2cb2d07d9260d04b5f
Njiraini23 <njiraini88@gmail.com>
2023-09-05 12:48:25 +0000

    0-simple_helper_function.py

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""A function that takes two integers page and size
+returnd a tuple of size two"""
+
+
+def index_range(page: int, page size: int) -> tuple[int, int]:
+    """ returns a tuple of size 2 """
+    if page < 1 or page_size <= 0:
+        pass
+
+    start_index = (page - 1) * page_size
+    end_index = (page * page_size)
+
+    return (start_index, end_index)
