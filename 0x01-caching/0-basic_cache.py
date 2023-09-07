d565c33f9c00e993d3fdb50a8f56cbe98913a359
Njiraini23 <njiraini88@gmail.com>
2023-09-07 09:51:45 +0000

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,13 +12,13 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """assigns a dictionary self.cache_data for
         the key"""
-        if key is None or Item is None:
+        if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
         """A method that returns self.cache_data"""
-        if key is not None and Key in self.cache_data.keys():
+        if key is not None and key in self.cache_data.keys():
             return self.cache_data[key]
         return None
