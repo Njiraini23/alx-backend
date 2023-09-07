192e9b1b17f277a7daea042f9c15062d365d3b00
Njiraini23 <njiraini88@gmail.com>
2023-09-07 20:08:39 +0000

    3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
-"""A class LRU that inherits from BaseCaching and
-is  a caching system"""
+"""containing LRUCache module"""
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """The class LRUCache that inherits from BaseCaching"""
+    """defining the class"""
     def __init__(self):
-        """initializes the class"""
+        """
+        initializing class by calling the constructor
+            of the parent class BaseCaching using super
+        initializing empty list self.usage to keep track of the
+            usage order of keys
+        """
         super().__init__()
         self.usage = []
 
     def put(self, key, item):
-        """assigns the item value of the key"""
-        if key is None and item is None:
+        """To store a key-value pair in the cache"""
+        if key is None or item is None:
             pass
         else:
             cache_length = len(self.cache_data)
@@ -24,16 +28,19 @@ class LRUCache(BaseCaching):
                 print('DISCARD: {}'.format(evicted_key))
 
             if key in self.usage:
-                """the key at the end of the list is the
-                most recently used"""
-                self.usage.remove
+                """
+                key moved to the end of the list
+                (indicating it's the most recently used)
+                from where its removed
+                """
+                self.usage.remove(key)
             self.usage.append(key)
             self.cache_data[key] = item
 
-        def get(self, key):
-            """returns the value in the cache_data linked to key"""
-            if key is None or key not in self.cache_data.keys():
-                return None
-            self.usage.remove(key)
-            self.usage.append(key)
-            return self.cache_data[key]
+    def get(self, key):
+        """return value in self.cache_data linked to key"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        self.usage.remove(key)
+        self.usage.append(key)
+        return self.cache_data[key]
