19676f91c95ff8eaac69f30dd86e69fe778474af
Njiraini23 <njiraini88@gmail.com>
2023-10-13 10:45:21 +0000

    changes

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,8 +3,8 @@ import { createQueue } from 'kue';
 const queue = createQueue();
 
 const notification = {
-  'phoneNumber': '123123123',
-  'message': 'please verify your email'
+  'phoneNumber': '4153518780',
+  'message': 'This is the code to verify your account'
 }
 
 const job = queue.create('push_notification_code', notification).save(function (err) {
