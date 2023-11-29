048077fa319f86206eb94ca390dd282d6a15087c
njiraini23 <njiraini88@gmail.com>
2023-11-29 08:09:23 +0000

    1-redis_op.js

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -6,7 +6,7 @@ function redisConnect() {
   client.on('connect', function() {
     console.log('Redis client connected to the server');
   }).on('error', (err) => {
-    console.log(`Redis client not conected to the server: ${err}`);
+    console.log(`Redis client not connected to the server: ${err}`);
   });
 
 };
