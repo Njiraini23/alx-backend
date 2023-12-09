852c0c6339132267c57949d2d7c4c1a8b2f80ee8
Njiraini23 <njiraini88@gmail.com>
2023-12-09 08:48:55 +0000

    2-app.py

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+'''Getting the locale request'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    '''a new class config'''
+
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieving the webpage locale
+
+    Returns:
+        str: best math
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    '''default route
+    Returns:
+        the homepage
+    '''
+
+
+if __name__ == "__main__":
+    app.run()
